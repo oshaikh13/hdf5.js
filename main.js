8740e75580254ba6d835cec5a96021250341db03
haehn <haehn@seas.harvard.edu>
2017-01-13 15:00:35 -0500

    Drag'n'drop HTML setup.

---
+++ main.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+  
+  // init DnD
+  document.getElementById("body").addEventListener("dragover", onDragOver, false);
+  document.getElementById("body").addEventListener("dragleave", onDragLeave, false);
+  document.getElementById("body").addEventListener("drop", onDrop, false);
+
+};
+
+function onDragOver(e) {
+  e.preventDefault();
+};
+
+function onDragLeave(e) {
+  e.preventDefault();
+};
+
+function onDrop(e) {
+
+  e.stopPropagation();
+  e.preventDefault();
+
+  // get the dropped files
+  var files = e.dataTransfer.files;
+
+  // if anything is wrong with the dropped files, exit
+  if (typeof files == "undefined" || files.length == 0) {
+    return;
+  }
+
+  console.log(files);
+
+};
