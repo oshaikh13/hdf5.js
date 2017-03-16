117f8fc84f36f6f4194f1029807de72bf14a4c59
Omar Shaikh <oshaikh13@gmail.com>
2017-03-16 08:14:04 +0300

    Add superblock handler

--- src/lowLevel.js
+++ src/lowLevel.js
@@ -1,10 +1,10 @@
 // Low level API's for HDF5 reader
                   // UINT   8
-function SuperBlock (bytes, offset) {
+function SuperBlock (bytes, start, end) {
 
   var superBlockObject = {};
 
-  var values = unpackStruct(SUPERBLOCK_V0, bytes, offset)
+  superBlockObject.contents = unpackStruct(SUPERBLOCK_V0, bytes, start);
 
   return superBlockObject;
 
