a8a1f04cc5ddbe124f0f97acce990fecf9063c1d
Omar Shaikh <oshaikh13@gmail.com>
2018-07-12 01:02:34 +0300

    Don't load the whole file into the buffer

--- src/highLevel.js
+++ src/highLevel.js
@@ -5,44 +5,34 @@ HDF5.File = function (file) {
   var fileObj = {};
 
   fileObj._file = file;
-  fileObj._reader = new FileReader();
   fileObj.SuperBlock = null;
-  fileObj._arrayBuffer = null;
 
-  /**
-   * The onError callback.
-   */
-  fileObj.onError = function(e) {
+  const readSuperBlock = (file, size, callback) => {
+    const superBlockReader = new FileReader();
+    superBlockReader.onloadend = (e) => {
+      if (e.target.readyState == FileReader.DONE) {
 
-    throw new Error(e.target.error.code);
-
-  };
-
-  /**
-   * The onLoad callback (really the onLoadEnd).
-   */
-  fileObj.onLoad = function(e) {
-
-    if (e.target.readyState == FileReader.DONE) {
-
-      // we read an array buffer
-      var loadedFile = e.target.result;
-
-      fileObj._arrayBuffer = new Uint8Array(loadedFile);
-
-      // and look at its superblock
-      this.SuperBlock = SuperBlock(fileObj._arrayBuffer, 0);
-      var offset = this.SuperBlock.offsetToDataObjects()[0];
-      // var dataObjects = DataObjects(fileObj._arrayBuffer, offset);
-
+        // we read an array buffer
+        var loadedFile = e.target.result;
+        const superBlockBuffer = new Uint8Array(loadedFile);
+  
+        // and look at its superblock
+        fileObj.SuperBlock = lowLevel.SuperBlock(superBlockBuffer, 0);
+        callback(false);  
+      }
     }
 
-  };
+    const superBlockBlob = file.slice(0, size + 1);
+    superBlockReader.readAsArrayBuffer(superBlockBlob);
+  }
 
-  fileObj._reader.onerror = fileObj.onError.bind(fileObj);
-  fileObj._reader.onloadend = fileObj.onLoad.bind(fileObj);
-
-  fileObj._reader.readAsArrayBuffer(fileObj._file);
+  readSuperBlock(file, consts.SUPERBLOCK_V0_SIZE + 
+                       consts.SYMBOL_TABLE_ENTRY_SIZE, 
+                       (err) => {
+    if (err) throw new Error("crap");
+    console.log(fileObj);
+    // var dataObjects = DataObjects(fileObj._arrayBuffer, offset);
+  }); 
 
   return fileObj;
 }
