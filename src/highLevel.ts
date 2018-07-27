f5ab78d3274a2bea09f85036a193620039f5d87a
Omar Shaikh <oshaikh13@gmail.com>
2018-07-27 16:21:10 +0300

    scaffold some more of HDF5 API

--- src/highLevel.ts
+++ src/highLevel.ts
@@ -2,6 +2,13 @@ import DataObjects from './DataObjects';
 import lowLevel from './lowLevel';
 import { DataObj, SuperBlock } from './interfaces';
 
+class Reference {
+  addressOfReference: number;
+  constructor(addressOfReference: number) { this.addressOfReference = addressOfReference };
+  isEmpty () : boolean {
+    return !!this.addressOfReference;
+  }
+}
 
 class Group {
   parent: HDF5File;
@@ -10,16 +17,68 @@ class Group {
 
   _links: object;
   _dataObjects: DataObj;
-  attrs: object;
+  _attrs: object;
 
-  setupGroup(name: string, dataObjects: DataObj, parent: HDF5File) {
+  setupGroup(name: string, dataObjects: DataObj, parent: HDF5File, callback) {
     this.name = name;
     this._dataObjects = dataObjects;
     this.parent = parent;
     this.file = parent._file;
-
-    this._links = dataObjects.getLinks();
+    dataObjects.getLinks((links) => {
+      this._links = links;
+      callback();
+    });
   }
+
+  length() : number {
+    return Object.keys(this._links).length;
+  }
+
+  toString () : string {
+    return "<HDF5 group " + this.name + " (" + this.length() + " members)>"
+  }
+
+  *_iter () : IterableIterator<string> {
+    for (var k in this._links) {
+      yield k;
+    }
+  }
+
+  _dereference (refr: Reference) : object {
+    if (refr.isEmpty()) {
+      throw new Error("Cannot dereference null");
+    }
+    const obj = this.parent._getObjectByAddress(refr.addressOfReference);
+    if (obj == null) {
+      throw new Error("Reference not found in file");
+    }
+    return obj;
+  }
+
+  get(y: any) {
+    if (y instanceof Reference) {
+      this._dereference(y);
+    }
+  }
+
+  visitItems (callback) : object {
+    // throw new Error("unimplemented");
+    let rootNameLength = this.name.length;
+
+    if (!this.name.endsWith('/')) {
+      rootNameLength++;
+    }
+
+    return null;
+  }
+
+  attrs () : object {
+    if (this._attrs == null) {
+      this._attrs = this._dataObjects.getAttributes();
+      return this._attrs;
+    } else return this._attrs;
+  }
+
 }
 
 class HDF5File extends Group {
@@ -31,9 +90,16 @@ class HDF5File extends Group {
     this._file = file;
     this.SuperBlock = lowLevel.SuperBlock(this, 0, (superBlock: SuperBlock) => {
       const dataObjects = DataObjects(this, superBlock._rootSymbolTable.groupOffset, (err) => {
-        this.setupGroup('/', dataObjects, this);
+        this.setupGroup('/', dataObjects, this, () => {
+
+        });
       });
     });
+  }
+
+  _getObjectByAddress(objAddress: number) {
+    if (this._dataObjects.offset === objAddress) return this;
+    return this.visitItems((x, y) => y._dataObjects.offset === objAddress);
   }
 }
 
